bbf12d51b437235b696d18807bfa63d21623bdc9
Zac <a19860101@gmail.com>
2019-09-02 13:37:34 +0800

    no

--- conn.php
+++ conn.php
@@ -6,7 +6,7 @@
         public $db_name="ntust";
 
         function connect(){
-            $conn = mysqli_connect($db_host,$db_user,$db_pw,$db_name)or die('mysql error');
+            $conn = mysqli_connect($this->db_host,$this->db_user,$this->db_pw,$this->db_name)or die('mysql error');
             mysqli_query($conn,"SET NAMES utf8");
             return $conn;
         }
