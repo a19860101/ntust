4d1b7d42e8679b8d9740728d110a723e5721c87e
Zac <a19860101@gmail.com>
2019-09-02 11:51:43 +0800

    db

--- conn.php
+++ conn.php
@@ -1,8 +1,16 @@
 <?php 
-    $db_host="localhost";
-    $db_user="admin";
-    $db_pw="admin";
-    $db_name="ntust";
+    class DB {
+        public $db_host="localhost";
+        public $db_user="admin";
+        public $db_pw="admin";
+        public $db_name="ntust";
 
-    $conn = mysqli_connect($db_host,$db_user,$db_pw,$db_name)or die('mysql error');
-    mysqli_query($conn,"SET NAMES utf8");
+        function connect(){
+            $conn = mysqli_connect($db_host,$db_user,$db_pw,$db_name)or die('mysql error');
+            mysqli_query($conn,"SET NAMES utf8");
+        }
+        return $conn;
+    }
+    
+
+    
