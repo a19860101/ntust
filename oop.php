5f6ab6975c98fc7c053f9732cda3cb496cc77d40
Zac <a19860101@gmail.com>
2019-09-02 11:40:04 +0800

    no

--- oop.php
+++ oop.php
@@ -2,18 +2,23 @@
     class Student {
         private $a = 100;
         public $b = 99;
+        protected $c = 66;
         function test(){
-            return $this->a + $this->b;
+            // return $this->a + $this->b;
         }
     }
 
     class Teacher extends Student{
-
+        function qq(){
+            echo $this->c;
+        }
     }
     $t = new Student;
     echo $t->test();
+    // echo $t->c;
 
-    // $test = new Teacher;
+    $test = new Teacher;
     // echo $test->a;
     // echo $test->test();
+    echo $test->qq();
 
